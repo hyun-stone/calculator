29679d91f22a478e71fd7d5631c1b7f18f27e3b5
hyun-stone <hello.git.congo1117@nate.com>
2024-06-27 23:08:42 +0900

    Add btn2(Clear) and handler function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # ch 4.2.1 main.py
 # 애플리케이션 구현에 필요한 라이브러리 추가
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit)      # QPlainTextEdit 추가
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)      # QHBoxLayout 추가
 from PyQt5.QtGui import QIcon       # icon을 추가하기 위한 라이브러리
 
 class Calculator(QWidget):      # QWidget 클래스를 상속받아서 클래스를 정의
@@ -17,10 +17,19 @@ class Calculator(QWidget):      # QWidget 클래스를 상속받아서 클래스
         self.btn1=QPushButton('Message', self)      # 버튼 추가
         self.btn1.clicked.connect(self.activateMessage)     # 버튼 클릭시 핸들러 함수 연결
 
+        self.btn2=QPushButton('Clear', self)        # 버튼 2 추가
+        self.btn2.clicked.connect(self.clearMessage)        # 버튼 2 핸들러 함수 연결
+
+        hbox = QHBoxLayout()        # 수평 박스 레이아웃을 추가하고 버튼1, 2 추가
+        hbox.addStretch(1)      # 공백
+        hbox.addWidget(self.btn1)       # 버튼 1 배치
+        hbox.addWidget(self.btn2)       # 버튼 2 배치
+
         vbox=QVBoxLayout()      # 수직 레이아웃 위젯 생성
         vbox.addWidget(self.te1)        # 수직 레이아웃에 텍스트 에디트 위젯 추가
 
-        vbox.addWidget(self.btn1)       # 버튼 위치
+        # vbox.addWidget(self.btn1)       # 버튼 위치
+        vbox.addLayout(hbox)        # btn1 위치에 hbox를 배치
         vbox.addStretch(1)      # 빈 공간
 
         self.setLayout(vbox)        # 빈 공간 - 버튼 - 빈 공간 순으로 수직 배치된 레이아웃 설정
@@ -29,6 +38,9 @@ class Calculator(QWidget):      # QWidget 클래스를 상속받아서 클래스
         self.setWindowIcon(QIcon('icon.png'))       # 윈도 아이콘 추가
         self.resize(256,256)        # 윈도 사이즈
         self.show()     # 윈도 화면이 표시되도록 호출
+
+    def clearMessage(self):     # 버튼 2 핸들러 함수 정의
+        self.te1.clear()
 
     def activateMessage(self):      # 핸들러 함수 수정 : 메시지가 텍스트 에디트에 출력되도록
         # QMessageBox.information(self, "information", "Button clicked!")
