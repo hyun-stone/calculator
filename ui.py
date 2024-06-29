2fabac214711844e5f87d6684d04b0d6883d8cb6
developer2 <dev2@email.com>
2024-06-29 23:28:45 +0900

    Modify btn1, le1 and method name to setDisplay in ui.py

--- ui.py
+++ ui.py
@@ -15,19 +15,21 @@ class View(QWidget):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
         
-        self.btn1=QPushButton('Message',self)
+        self.btn1=QPushButton('Calc',self)      # 버튼 이름 변경
         self.btn2=QPushButton('Clear',self)
         
-        self.le1=QLineEdit('0',self)        # 라인 에디트1 추가
-        self.le1.setAlignment(QtCore.Qt.AlignRight)     # 라인 에디트1 문자열 배치 설정
+        self.le1=QLineEdit('0',self)
+        self.le1.setAlignment(QtCore.Qt.AlignRight)
+        self.le1.setFocus(True)     # 포커스 설정
+        self.le1.selectAll()        # 텍스트 전체 선택
         
-        self.le2=QLineEdit('0',self)        # 라인 에디트2 추가
-        self.le2.setAlignment(QtCore.Qt.AlignRight)     # 라인 에디트2 문자열 배치 설정
+        self.le2=QLineEdit('0',self)
+        self.le2.setAlignment(QtCore.Qt.AlignRight)
         
-        self.cb = QComboBox(self)       # 콤보 박스 추가
-        self.cb.addItems(['+', '-', '*', '/'])      # 콤보 박스 항목 추가(연산자로 사용)
+        self.cb = QComboBox(self)
+        self.cb.addItems(['+', '-', '*', '/'])
         
-        hbox_formular = QHBoxLayout()       # 새로 정의한 위젯을 QHBoxLayout에 배치
+        hbox_formular = QHBoxLayout()
         hbox_formular.addWidget(self.le1)
         hbox_formular.addWidget(self.cb)
         hbox_formular.addWidget(self.le2)
@@ -39,7 +41,7 @@ class View(QWidget):
         
         vbox=QVBoxLayout()
         vbox.addWidget(self.te1)
-        vbox.addLayout(hbox_formular)       # hbox_formular 배치
+        vbox.addLayout(hbox_formular)
         vbox.addLayout(hbox)
         vbox.addStretch(1)
         
@@ -50,7 +52,7 @@ class View(QWidget):
         self.resize(256,256)
         self.show()
         
-    def activateMessage(self):
+    def setDisplay(self):       # 메서드 이름 변경
         self.te1.appendPlainText("Button clicked!")
         
     def clearMessage(self):
